e2d93dab82011789b1128e09bbf20edb1bca3984
root <root@e-bibobo-m6rmq-0.e-bibobo-m6rmq.ace-hub.svc.cluster.local>
2023-05-03 21:13:55 +0000

    initial upload

---
+++ lab6.md
@@ -0,0 +1,40 @@
+# Lab 6
+This lab will once again be a lab from scratch which means we will have to do the seven-step process again. Our problem this time will be to determine the fandom of fan fictions. We will use the text of the fan fiction (the story) to determine which fandom it belongs to. We will use some flavor of RNNs to treat the words as a sequence instead of doing statistics on the sentence. 
+
+## Step 1 Get Some Data
+The instructor will provide the fan fiction data scraped from the site [Archive of Our Own](https://archiveofourown.org/) (AO3). This data was obtained with permission from the site to use for academic and educational purposes. However, if you plan to publish text from any specific fan fiction you must obtain consent from the author. Also, if you did not already know fan fictions can get weird so read the actual fan fictions at your own risk.  
+
+The data is available on Canvas under the lab assignment. Use the small dataset `website_txt_splits_small.zip` for this lab (the large dataset is takes a lot longer to process, but you can try it if you wish). After unzipping, there will be a train and test folder. The train and test folders will have directories for each fandom. Each fandom folder will contain `.txt` files with the actual fan fiction story. You can expect only english language but potentially odd unicode characters or special symbols. 
+
+## Step 2 Measures of Success
+Our problem this time is of multi class classification thus we are going to use `accuracy` as our metric. A note on the data, fan fictions are known for "crossovers" which have more than one fandom in the same story. When the data was created, if a fan fiction was in both fandoms it was copied to both folders for the respective fandoms. Thus, some stories will actually belong to two fandoms but are labelled as only one. Trying to determine if stories actually belong to both fandoms is beyond the scope of this lab.
+
+Our baseline will be a model made by the instructor that did not use sequential data. This is a sensible baseline since this is the type of model that would be used if we did not know of RNNs. The baseline was a "linear regression" (technically a multivariate linear regression) model trained on word counts in the story. For reference, this baseline model achieved an accuracy of about 85% on the given test set. 
+
+## Step 3 Prepare Data
+Prepare you text data so that it can be ingested by an RNN. This means you need to make a tensor with three dimensions corresponding to the sample, time step (or more generally position in sequence), and finally data. Remember there are many representations for words (tokens) such as sparse (one hot), or dense (an embedding). Also, which words are included in the vocabulary is up to the student. 
+
+I suggest using the function `text_dataset_from_directory` from `tensorflow.keras.preprocessing`. This can generally make working with a large number of text files much easier. This [site](https://www.tensorflow.org/api_docs/python/tf/keras/preprocessing/text_dataset_from_directory) is the docs for `text_dataset_from_directory` which may prove useful. Also, the keras layer `TextVectorization` with docs [here](https://www.tensorflow.org/api_docs/python/tf/keras/layers/experimental/preprocessing/TextVectorization) may also be useful. 
+
+## Step 4 Evaluation Method
+The dataset contains a separate training and test set. You may use the training set for your validation splits as you see necessary. 
+
+Remember to report your final accuracy on the test set which you only did after you decided your model had trained well enough.
+
+## Step 5 Develop a model
+In this section make a model that uses recurrent layers in some fashion. At this point you just want to get the model to compile. 
+
+Separate this code from the next section with comments or if statements when you get a compiling model.
+
+## Step 6 Overfit Model
+Add more layers or other techniques to overfit the model to your training data. Note that RNNs can be much slower to train compared to CNNs or dense ANNs. Using GPU implementations of LSTM will be much faster and is automatically done if under certain LSTM arguments, see the [docs](https://www.tensorflow.org/api_docs/python/tf/keras/layers/LSTM) This problem is not complex enough to require training for a very long time and thus your final model should be able to train in < 10 minutes. This is not a hard requirement, but it will help you to try out more models and save the instructor time in grading. 
+
+Again separate this code from the earlier code and later code by comments or if statements.
+
+## Step 7 Regularize Model
+Once the model overfits, you can then add regularization to achieve acceptable performance on your validation data. 
+
+Again separate this model from the rest. If this model is the exact same as the one before it then note that in your code.
+
+## Test Performance and Visualization
+After you have a satisfactory model evaluate it against the test set and note the performance in a comment in the code. Also use the visualization code in the template to show the confusion matrices for the different classes. 
---
+++ src/lab6.py
@@ -0,0 +1,226 @@
+import csv
+import itertools
+import os
+import typing
+from pathlib import Path
+
+import h5py
+import matplotlib.pyplot as plt
+import numpy as np
+import sklearn
+import sklearn.metrics
+import tensorflow as tf
+import tensorflow.keras.preprocessing.text
+from matplotlib.axes import Axes
+from tensorflow.keras.layers import Input, Dense, Embedding, LSTM
+from tensorflow.keras.layers.experimental.preprocessing import TextVectorization
+from tensorflow.keras.models import Model, load_model
+from tensorflow.keras.preprocessing import text_dataset_from_directory
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.utils import to_categorical
+from tensorflow.python.keras.callbacks import TensorBoard
+from tensorflow.python.keras.layers import Dropout
+from tqdm import tqdm
+from tqdm.keras import TqdmCallback
+
+# Make into Path objects instead? 
+
+fig_path = '/remote_home/EENG645a-Sp23/Lab6/Lab6-template/figures'
+model_path = '/remote_home/EENG645a-Sp23/Lab6/Lab6-template/models'
+log_path = '/remote_home/EENG645a-Sp23/Lab6/Lab6-template/logs'
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.savefig(f'{fig_path}/{title}')
+
+
+def write_vocabulary(vocabulary_filename: Path, vocabulary: typing.List[str]):
+    pass
+
+
+def read_vocabulary(vocabulary_filename: Path) -> typing.List[str]:
+    vocabulary = []
+    return vocabulary
+
+
+def get_dataset(data_dir: Path,
+                vocabulary_filename: Path,
+                max_words: int,
+                max_token_length: int,
+                batch_size: int,
+                validation_split: float,
+                ) -> typing.Tuple[tf.data.Dataset, tf.data.Dataset, typing.List[str]]:
+    # make dataset objects using text_dataset_from_directory
+    train_dataset: tf.data.Dataset
+    validation_dataset: tf.data.Dataset
+
+    # check our categories and number of classes
+    categories = train_dataset.class_names
+    num_classes = len(categories)
+
+    # process the raw text into tokens. remember to remove punctuation
+    # you may want to load your saved vocabulary into the layer here (if a saved vocabulary exists)
+    vectorize_layer: TextVectorization
+
+    # determine and save vocabulary for later use
+    if not os.path.exists(vocabulary_filename):
+        # get vocabulary
+        write_vocabulary(vocabulary_filename=vocabulary_filename, vocabulary=vectorize_layer.get_vocabulary())
+    else:
+        # if we didn't already load the vocabulary to the vectorize_layer do it now
+        print("reading in saved vocabulary")
+        vocabulary = read_vocabulary(vocabulary_filename=vocabulary_filename)
+        vectorize_layer.set_vocabulary(vocab=np.asarray(vocabulary))
+
+    # do any other work to process dataset.
+    # For example, you could apply your vectorize layer here on the text input stream
+    # Also  you could process the output classes to be one hots if necessary
+    # Remember at this point you data should be shuffled, batched, scaled, repeated, prefetched, cached or
+    # anything else you want in your pipeline
+
+    # print out a sample to make sure it looks alright
+    sample = next(train_ds.as_numpy_iterator())
+    print(f"Input shape: {sample[0].shape}, Output shape: {sample[1].shape}")
+    print(f"First sample raw input:\n{sample[0][0]}")
+    # if you used an integer encoding on the text we can decode it to get the sentence with our vocabulary
+    vocabulary_list: typing.List[str] = vectorize_layer.get_vocabulary()
+    text_list: typing.List[str] = []
+    for word_index in sample[0][0]:
+        text_list.append(vocabulary_list[word_index])
+    print(f"First sample input decoded:\n{' '.join(text_list)}")
+    print(f"First sample label: {sample[1][0]}")
+
+    return train_dataset, validation_dataset, categories
+
+
+def sample_from_dataset(dataset: tf.data.Dataset,
+                        eval_samples=6000):
+    """
+    Uses a model and a dataset to visualize the current predictions
+
+    :param dataset: the dataset to use for generating predictions and comparing against truth
+    :param eval_samples: the number of samples used to evaluate from the dataset
+    """
+    x_visualize = []
+    y_visualize = []
+
+    total_samples = 0
+    img_batch: np.ndarray
+    label_batch: np.ndarray
+    for img_batch, label_batch in dataset.as_numpy_iterator():
+        total_samples += img_batch.shape[0]
+        x_visualize.append(img_batch)
+        y_visualize.append(label_batch[:, None])
+        if total_samples > eval_samples:
+            break
+
+    x_visualize = np.vstack(x_visualize)
+    y_visualize = np.vstack(y_visualize)
+
+    x_visualize = np.squeeze(x_visualize)
+    y_visualize = np.squeeze(y_visualize)
+
+    return x_visualize, y_visualize
+
+
+def main():
+    base_data_dir = Path('/opt', 'data', 'FanFic', f'website_txt_splits_small')
+    train_data_dir = base_data_dir / 'train'
+    test_data_dir = base_data_dir / 'test'
+
+    force_fit_model: bool
+
+    validation_split: float
+
+    vocabulary_filename = Path('vocabulary.tsv')
+    model_name = 'model.h5'
+
+    batch_size: int
+
+    # format the input sequences
+    max_token_length: int
+    max_words: int
+
+    train_ds, valid_ds, categories = get_dataset(data_dir=train_data_dir,
+                                                 vocabulary_filename=vocabulary_filename,
+                                                 max_words=max_words,
+                                                 max_token_length=max_token_length,
+                                                 batch_size=batch_size,
+                                                 validation_split=validation_split)
+
+    if force_fit_model or not os.path.exists(model_name):
+
+        # make model using the dataset
+        model: Model
+
+        # save the model
+        model.save(model_name)
+
+    else:
+        # load the saved model
+        model = load_model(model_name)
+
+    # get some samples from our dataset
+    x_test, y_test = sample_from_dataset(dataset=train_ds,
+                                         eval_samples=6000)
+
+    # do prediction
+    y_true: np.ndarray
+    y_pred: np.ndarray
+
+    # print the visualization metrics
+    class_names = [cat[:15] for cat in categories]
+
+    print(sklearn.metrics.classification_report(y_true, y_pred, target_names=class_names))
+
+    confusion_matrix = sklearn.metrics.confusion_matrix(y_pred=y_pred,
+                                                        y_true=y_true)
+
+    np.set_printoptions(precision=2)
+
+    # Plot non-normalized confusion matrix
+    plt.figure()
+    plot_confusion_matrix(confusion_matrix, classes=class_names,
+                          title='Confusion matrix, without normalization')
+
+    # Plot normalized confusion matrix
+    plt.figure()
+    plot_confusion_matrix(confusion_matrix, classes=class_names, normalize=True,
+                          title='Normalized confusion matrix')
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
